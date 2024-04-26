6d2227fb92eada7d3ea3a816dcdd292d1c420fb0
igregorioneto <igregoriodoneto@outlook.com>
2024-04-26 20:16:00 -0300

    feat: UI project

---
+++ UI/Assets/Scripts/AreaInterativa.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaInterativa : MonoBehaviour
+{
+    // Variáveis
+    public string[] possiveisMensagens;
+    public AudioClip somInterativo;
+
+    // Métodos
+
+    // Métodos Internos da Unity
+    private void OnTriggerEnter(Collider other)
+    {
+        // Verificação se quem entrou na área interativa é de fato o jogador!
+        if(other.gameObject.tag == "Player")
+        {
+            if (somInterativo != null)
+                AudioSource.PlayClipAtPoint(somInterativo, gameObject.transform.position, 1f);
+            int mensagemEscolhida = Random.Range(0, possiveisMensagens.Length);
+            Debug.Log("Mensagem: " + possiveisMensagens[mensagemEscolhida]);
+        }
+    }
+}
---
+++ UI/Assets/Scripts/CameraSeguidora.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSeguidora : MonoBehaviour
+{
+    public Transform alvoASeguir;
+    public float tempoOlhar, tempoAnimacao;
+
+    void Update()
+    {
+        iTween.MoveUpdate
+            (
+                gameObject,     // Quem é o objeto que irá realizar o método
+                iTween.Hash     // Configurações
+                (
+                    "position", alvoASeguir.position,
+                    "looktarget", alvoASeguir.parent,
+                    "looktime", tempoOlhar,
+                    "time", tempoAnimacao
+                )
+            );
+    }
+}
