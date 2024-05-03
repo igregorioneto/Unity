6f41f23c098cce7fa93b4246ecee5f66da6f1b6c
igregorioneto <igregoriodoneto@outlook.com>
2024-05-03 16:28:38 -0300

    feat: Game manager

--- Games/Platform/Assets/Scripts/GameManager.cs
+++ Games/Platform/Assets/Scripts/GameManager.cs
@@ -1,18 +1,100 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Sprite[] spritesVida;
+    [SerializeField] Image[] imagensVida;
+    [SerializeField] GameObject telaVitoria, telaDerrota;
+    [SerializeField] Text textoPontuacao;
+
+    private int vidas = 9;
+    private int pontuacao = 0;
+    private bool poderTomarDano = true;
+
+    private static GameManager _instance;
+    public static GameManager Instance { get { return _instance; } }
+
+    private void Awake()
     {
-        
+        _instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        vidas = 9;
+        pontuacao = 0;
+        textoPontuacao.text = $"{pontuacao}";
+        poderTomarDano = true;
+
+        AtualizarImagensVida();
+
+        telaVitoria.SetActive(false);
+        telaDerrota.SetActive(false);
+    }
+
+    public void CapturarMoeda()
+    {
+        pontuacao += 1;
+        textoPontuacao.text = $"{pontuacao}";
+    }
+
+    public void Derrota()
+    {
+        telaDerrota.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Ganhou()
+    {
+        telaVitoria.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void TomarDano()
+    {
+        if (!poderTomarDano)
+           return;
+        poderTomarDano = false;
+        Invoke("PoderTomarDano", 1.5f);
+
+        vidas--;
+        if (vidas > 0)
+        {
+            AtualizarImagensVida();
+        }
+        else
+        {
+            Derrota();
+        }
+    }
+
+    private void PoderTomarDano()
+    {
+        poderTomarDano = true;
+    }
+
+    private void AtualizarImagensVida()
+    {
+        if (vidas > 6)
+        {
+            imagensVida[0].sprite = spritesVida[2];
+            imagensVida[1].sprite = spritesVida[2];
+            imagensVida[2].sprite = spritesVida[vidas - 7];
+        }
+        else if (vidas > 3)
+        {
+            imagensVida[0].sprite = spritesVida[2];
+            imagensVida[1].sprite = spritesVida[vidas - 4];
+            imagensVida[2].sprite = spritesVida[0];
+        }
+        else if (vidas > 0)
+        {
+            imagensVida[0].sprite = spritesVida[vidas - 1];
+            imagensVida[1].sprite = spritesVida[0];
+            imagensVida[2].sprite = spritesVida[0];
+        }
     }
 }
